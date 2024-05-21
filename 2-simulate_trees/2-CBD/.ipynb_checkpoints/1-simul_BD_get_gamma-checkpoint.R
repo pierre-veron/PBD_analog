b574e13f6592be75e18b2b4baad3d9f6de0e36f8
Jérémy Andréoletti <45073058+Jeremy-Andreoletti@users.noreply.github.com>
2024-05-21 11:02:58 +0200

    Stem VarBD simulations

---
+++ 2-simulate_trees/2-CBD/.ipynb_checkpoints/1-simul_BD_get_gamma-checkpoint.R
@@ -0,0 +1,67 @@
+library(ape)
+library(treestats)
+
+setwd("C:\Users\pveron\Documents\GitHub\PBD_analog")
+
+set.seed(394)
+
+age <- 15
+
+equivalent_bd_rates <- function(param) {
+  l1 <- param[1]
+  l2 <- param[2]
+  l3 <- param[3]
+  m1 <- param[4]
+  m2 <- param[5]
+  p <- 0.5*(l2+l3+m2)/l3 * (1-sqrt(1-4*l3*m2/((l2+l3+m2)^2)))
+  l <- (1-p)*l1
+  m <- m1
+  rates <- c(l, m)
+  names(rates) <- NULL
+  rates
+}
+
+simul_infer <- read.csv("simulations_output/PBD/all_simulations_inference.csv")
+
+param_PBD_names <-  paste0("PBD.", c("l1", "l2", "l3", "mu1", "mu2"))
+
+
+# Case of simulations where only one parameter varies
+if ("param_vary" %in% colnames(simul_infer)) {
+  col_to_keep <- c("param_vary", "i_param_var", "replicate", param_PBD_names)
+} else {
+  col_to_keep <- c("i_param_var", "replicate", param_PBD_names)
+}
+
+
+
+
+tree_stats_df <- as.data.frame(t(sapply(rownames(simul_infer), function(rw) {
+  param_PBD <- unlist(simul_infer[rw, param_PBD_names])
+  eq_bd <- equivalent_bd_rates(param_PBD)
+  tryCatch(
+    {
+    tree <- ape::rbdtree(eq_bd[1], eq_bd[2], age, eps = 1e-04)
+    fname <- paste0("simulations_output/2-CBD/trees/CBD_tree_sim_", rw, "_b_", eq_bd[1], "_d_", eq_bd[2],  ".nwk")
+    ape::write.tree(tree, fname)
+    out <- c(unlist(simul_infer[rw, col_to_keep]),
+      "gamma" = ape::gammaStat(tree),
+      "stairs2" = treestats::stairs2(tree),
+      "SR" = length(tree$tip.label),
+      "equiv_birth" = eq_bd[1],
+      "equiv_death" = eq_bd[2])
+    out
+    },
+    error = function(e) {
+      out <- c(unlist(simul_infer[rw, col_to_keep]),
+               "gamma" = NA,
+               "SR" = NA,
+               "stairs2" = NA,
+               "equiv_birth" = eq_bd[1],
+               "equiv_death" = eq_bd[2])
+      out
+    }
+  )
+})))
+
+write.csv(tree_stats_df, "simulations_output/2-CBD/simulated_BD_trees_stats.csv")
