70f294a27513f7a22077e6427ee39f13d62ae8e6
Jérémy Andréoletti <45073058+Jeremy-Andreoletti@users.noreply.github.com>
2024-06-24 14:19:05 +0200

    B and D in figure S7 labels

--- 4-fit_BD_truncated_PBD_trees/3-plot_comparison.R
+++ 4-fit_BD_truncated_PBD_trees/3-plot_comparison.R
@@ -40,6 +40,8 @@ create_plot <- function(data, y_var, x_label, y_label, eq_var) {
     )
 }
 
+manual_trans <- scales::trans_new("tanh", transform = function(x) tanh(x*1.5), inverse = function(y) atanh(y)/1.5)
+
 create_plot <- function(data, y_var, x_label, y_label, eq_var) {
   # Calculate the median of the filtered data for each split time
   median_data <- data %>%
@@ -47,15 +49,15 @@ create_plot <- function(data, y_var, x_label, y_label, eq_var) {
     summarise(median_value = median(!!sym(y_var), na.rm = TRUE))
   
   # Filter out high values (some inferred rates explode)
-  filtered_data <- data[data[y_var] < 3,]
+  filtered_data <- data[data[y_var]<100,]
   
   ggplot(filtered_data, aes(x = split_times, y = !!sym(y_var))) +
-    geom_jitter(alpha = 0.1, col = brewer.pal(n = 7, name = "YlOrBr")[i_var + 2]) +
-    #geom_line(alpha = 0.1, col = brewer.pal(n = 7, name = "YlOrBr")[i_var + 2], linetype = replicate) +
+    #geom_jitter(alpha = 0.03, col = brewer.pal(n = 7, name = "YlOrBr")[i_var + 2]) +
+    geom_line(aes(group = replicate), alpha = 0.1, col = brewer.pal(n = 7, name = "YlOrBr")[i_var + 2], show.legend = FALSE) +
     geom_hline(aes(yintercept = !!sym(eq_var))) +
-    geom_line(data = median_data, aes(x = split_times, y = median_value), linetype = "dotdash", color = "darkblue", lwd=0.8) +  # Add the median trend line
+    geom_line(data = median_data, aes(x = split_times, y = median_value), linetype = "dotdash", color = "blue4", lwd=1) +  # Add the median trend line
     scale_x_continuous(trans = "pseudo_log") +
-    scale_y_continuous(trans = "sqrt", limits = c(0.0, NA)) +
+    scale_y_continuous(transform = manual_trans, breaks = c(0,0.1,0.2,0.3,0.5,1,5), limits = c(0,NA)) +
     labs(title = plot_title, x = x_label, y = y_label) + 
     theme_tufte() +
     theme(
@@ -80,8 +82,8 @@ for (i_var in unique_i_param_var) {
     subset_df <- long_df %>% filter(param_vary == param_var, i_param_var == i_var)
     
     x_label <- if (i_var == 5) "Time of truncation" else ""
-    y_label_lambda <- if (param_var == 1 & i_var == 3) "Inferred Speciation Rate" else ""
-    y_label_mu <- if (param_var == 1 & i_var == 3) "Inferred Extinction Rate" else ""
+    y_label_lambda <- if (param_var == 1 & i_var == 3) "Inferred Birth Rate" else ""
+    y_label_mu <- if (param_var == 1 & i_var == 3) "Inferred Death Rate" else ""
     plot_title <- value_labels(i_var, param_var)
     
     # Create the plot for lamb_par
